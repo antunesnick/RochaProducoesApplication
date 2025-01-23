41b85868a8704aee640f4e58586df772a62efce2
nickolas <nickolasalmeida2@gmail.com>
2025-01-23 08:50:45 -0300

    start spring project

---
+++ RochaProducoes/src/main/java/com/nickolas/RochaProducoes/RochaProducoesApplication.java
@@ -0,0 +1,13 @@
+package com.nickolas.RochaProducoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RochaProducoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RochaProducoesApplication.class, args);
+	}
+
+}
---
+++ RochaProducoes/src/test/java/com/nickolas/RochaProducoes/RochaProducoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickolas.RochaProducoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RochaProducoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
