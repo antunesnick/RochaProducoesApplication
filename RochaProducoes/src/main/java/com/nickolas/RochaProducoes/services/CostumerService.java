5f0b727b20f7948d4abfa8fed53e314fe66a91a1
nickolas <nickolasalmeida2@gmail.com>
2025-01-26 02:55:24 -0300

    feat: create costumer service

--- RochaProducoes/src/main/java/com/nickolas/RochaProducoes/services/CostumerService.java
+++ RochaProducoes/src/main/java/com/nickolas/RochaProducoes/services/CostumerService.java
@@ -2,25 +2,19 @@ package com.nickolas.RochaProducoes.services;
 
 import com.nickolas.RochaProducoes.models.Costumer;
 import com.nickolas.RochaProducoes.repositories.CostumerRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriBuilder;
-
 
 @Service
 public class CostumerService {
 
     private final CostumerRepository costumerRepository;
 
-    public CostumerService(final CostumerRepository costumerRepository) {
+    CostumerService(CostumerRepository costumerRepository) {
         this.costumerRepository = costumerRepository;
     }
 
-
-    public void createCostumer(Costumer costumer) {
-        costumerRepository.save(costumer);
+    public Costumer createCostumer(Costumer costumer) {
+        return costumerRepository.save(costumer);
     }
-
-
 
 }
