f6c9d9daa70b282f51f1bf0bf002ddafd178b89b
nickolas <nickolasalmeida2@gmail.com>
2025-01-28 21:56:47 -0300

    feat: implement integration with viacep

--- RochaProducoes/src/main/view/login/app.js
+++ RochaProducoes/src/main/view/login/app.js
@@ -1,3 +1,53 @@
+function limpa_cep() {
+    document.getElementById("address").value="";
+    document.getElementById("city").value="";
+    document.getElementById("state").value="";
+
+}
+
+function valida_cep(inputCep) {
+    if(!/^\d{8}$/.test(inputCep)) {
+        return false;
+    }
+    return true;
+}
+
+
+function preenche_cep() {
+    
+    limpa_cep();
+    const inputCep = document.getElementById("cep").value.trim();
+    if(!valida_cep(inputCep)){
+        alert("CEP inválido! Digite um cep com 8 números apenas.");
+        return;
+    }
+
+    const url = `https://viacep.com.br/ws/${inputCep}/json/`;
+    fetch(url).then(response => {
+        if(!response.ok){
+            throw new Error("Erro ao buscar o cep,");
+        }
+        return response.json();
+    }).then(data => {
+        if(data.erro) {
+            alert("Cep não encontrado.");
+            return;
+        }
+        document.getElementById("address").value = data.logradouro || "";
+        document.getElementById("city").value= data.localidade || "";
+        document.getElementById("state").value= data.estado || "";
+    }).catch(error => {
+        console.error("Erro na requisição", error);
+        alert("Erro ao buscar o CEP! tente novamente.");
+    });
+    
+}
+
+
+
+
+
+
 document.getElementById("formCadastro").addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -38,3 +88,5 @@ document.getElementById("formCadastro").addEventListener('submit', function(even
         console.error("Erro: ", error);
     });
 });
+
+
