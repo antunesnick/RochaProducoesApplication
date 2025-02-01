9b976fd81fd3986f40ee91d6c090cfc981945b98
nickolas <nickolasalmeida2@gmail.com>
2025-02-01 14:32:47 -0300

    feat: add gets and setters in user model

--- RochaProducoes/src/main/java/com/nickolas/RochaProducoes/models/User.java
+++ RochaProducoes/src/main/java/com/nickolas/RochaProducoes/models/User.java
@@ -27,11 +27,35 @@ public class User {
     @NotBlank(message = "Campo em branco.")
     private String email;
 
+    public UUID getUserId() {
+        return userId;
+    }
 
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
 
+    public String getUserName() {
+        return userName;
+    }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
